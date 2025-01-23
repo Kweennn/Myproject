447319e23fc91d5bcf683cd506c9f9f5bc8329a4
KayceVergara <vergarakayce24@gmail.com>
2025-01-24 00:01:24 +0800

    first commit

---
+++ src/components/hero.jsx
@@ -0,0 +1,37 @@
+import me from '../assets/me.png'
+import '../App.css'
+
+function Hero() {
+    return (
+      <div className="hero-container">
+        <header className="hero-header">
+          <div className="hero-logo">Queen</div>
+          <nav>
+            <ul className="hero-nav-links">
+              <li>Home</li>
+              <li>About Me</li>
+              <li>Projects</li>
+              <li>CV</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        </header>
+  
+        <div className="hero-content">
+        <div className="hero-text">
+          <h1>Queenie Jane Canoy</h1>
+          <h2>UI / UX Designer</h2><br/>
+          <p> I craft seamless, user-focused interfaces that deliver exceptional digital experiences and lasting impressions.</p>
+          <button className="hero-cv-button">Learn More</button>
+        </div>
+
+        <div className="hero-image-container">
+          <img src={me} alt="Hero" className="hero-image" />
+        </div>
+      </div>
+      </div>
+    );
+  }
+  
+  export default Hero;
+
